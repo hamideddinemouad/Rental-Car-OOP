2ecf3a16ed9b3a0f16b0b4b41e18fe339673d505
dounia-elg <dounia.elg20@gmail.com>
2024-12-26 10:32:01 +0100

    save

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Rental</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <!-----------------Header----------------------->
+    <nav class="header">
+        <div class="logo">
+            <img src="../img/logooo.png" href="./home.html" alt="car Logo">
+        </div>
+        <div class="burger-menu" id="burgerMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="nav-menu" id="navMenu">
+            <ul>
+                <li><a href="./home.html" class="active">Home</a></li>
+                <li><a href="./Clients/clients.php">Clients</a></li>
+                <li><a href="./Cars/cars.php">Cars</a></li>
+                <li><a href="./Contrats/contrats.php">Contrats</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-----------------Hero----------------------->
+    <section class="hero">
+        <h1>Your Next Adventure <br><span>Starts Here</span></h1>
+        <p>Rent Your <span>Dream Car</span> Today!</p>
+    </section>
+
+    <script src="./script.js"></script>
+</body>
+</html>
