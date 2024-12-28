d49af6e0ccf6f2fe8a097485c86a31fc8ef686fa
dounia-elg <dounia.elg20@gmail.com>
2024-12-29 00:26:46 +0100

    Create a validator class

--- Authentification/login.php
+++ Authentification/login.php
@@ -1,3 +1,10 @@
+<?php
+require_once '../Validator.php'; 
+require_once '../classAuth.php';
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,20 +14,29 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="flex items-center justify-center min-h-screen bg-black">
-    <form method="POST" action="../classAuth.php" class="bg-white p-6 rounded-lg shadow-md w-full max-w-xl">
+    <form method="POST" class="bg-white p-6 rounded-lg shadow-md w-full max-w-xl">
         <input type="hidden" name="action" value="login">
 
         <h2 class="text-2xl font-bold text-yellow-500 mb-4 text-center">Login</h2>
+
+        <?php if (isset($errorMessage)): ?>
+            <div class="mb-4 text-red-500 text-center">
+                <?= htmlspecialchars($errorMessage) ?>
+            </div>
+        <?php endif; ?>
+
         <div class="mb-4">
             <label for="email" class="block text-sm font-medium text-gray-600">Email</label>
-            <input type="email" name="email" id="email" placeholder="Your email" required class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-red-600">
+            <input type="email" name="email" id="email" placeholder="Your email" class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-red-600">
         </div>
+
         <div class="mb-6">
             <label for="password" class="block text-sm font-medium text-gray-600">Password</label>
-            <input type="password" name="password" id="password" placeholder="Your password" required class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-red-600">
+            <input type="password" name="password" id="password" placeholder="Your password" class="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-red-600">
         </div>
+
         <button type="submit" class="w-full bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600">Login</button>
-        <p class="mt-4 text-sm text-center text-gray-600">Don't have an account? <a href="../Authentification/register.php" class="text-red-500 hover:underline">Register here</a></p>
+        <p class="mt-4 text-sm text-center text-gray-600">Don't have an account? <a href="register.php" class="text-red-500 hover:underline">Sign up here</a></p>
     </form>
 </body>
 </html>
